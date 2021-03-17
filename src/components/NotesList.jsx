7f1762f66dcd21e8889c1e60db884c76cb13fb8e
irma leandre <irmaleandre@irmas-MBP.PK5001Z>
2021-03-17 10:40:38 -0400

    basic template for NoteList

--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -1,6 +1,25 @@
+import { useState, useEffect } from 'react'
+
 function NotesList() {
-  return(
-<h1>Notes List</h1>
+  const [title, setTitle] = useState("")
+  const [time, setTime] = useState("")
+  const [description, setDescription] = useState("")
+
+  return (
+    <div>
+    <h1>Notes List</h1>
+    <form>
+      <label>Title: 
+        <input type="text" value={title} />
+      </label>
+      <label>Time: 
+        <input type="time" value={time} />
+      </label>
+      <label>Description: 
+        <textarea type="textarea" value={description} />
+      </label>
+    </form>
+    </div>
   )
 }
 
