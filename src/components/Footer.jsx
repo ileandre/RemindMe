4a0895b37437f22a88b9e87b47925f2f2c623778
irma leandre <irmaleandre@irmas-Air.PK5001Z>
2021-06-02 20:03:09 -0400

    background color change and copy right

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer () {
+    return(
+<div className="footer">
+<p className="copyright">&copy; 2021 Irma Leandre</p>
+</div>
+    )
+}
+
+export default Footer
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useHistory } from "react-router-dom";
 import { notesBaseURL, config } from "../services";
 import { useState } from "react";
+import Footer from "./Footer";
 
 function Homepage(props) {
   const { setMonth, setNumberOfDays, setStartDay, setYear } = props;
@@ -63,97 +64,100 @@ function Homepage(props) {
 
   return (
     <div className="homepage">
-      <div className="homeTitle">
-        <h1>Welcome to</h1>
-        <span className="remind">Remind</span>
-        <span className="me">Me</span>
-      </div>
-      <p>
-        Choose the month, number of days in that month, its start day and the
-        year you'd like to plan out.
+      <div className="mainInfo">
+        <div className="homeTitle">
+          <h1>Welcome to</h1>
+          <span className="remind">Remind</span>
+          <span className="me">Me</span>
+        </div>
+        <p>
+          Choose the month, number of days in that month, its start day and the
+          year you'd like to plan out.
       </p>
 
-      <form onSubmit={handleSubmit}>                                {/* all the arrays are mapped out to create the filters the user will use to create their calendar */}
-        <div className="homepageFilters">
-          <select
-            defaultValue="Month"
-            name="filter"
-            id="monthFilter"
-            onChange={(e) => setMonth(e.target.value)}
-          >
-            <option disabled value="Month">
-              Month
+        <form onSubmit={handleSubmit}>                                {/* all the arrays are mapped out to create the filters the user will use to create their calendar */}
+          <div className="homepageFilters">
+            <select
+              defaultValue="Month"
+              name="filter"
+              id="monthFilter"
+              onChange={(e) => setMonth(e.target.value)}
+            >
+              <option disabled value="Month">
+                Month
             </option>
-            {monthsArr.map((month, index) => {
-              return (
-                <option key={index} value={month}>
-                  {month}
-                </option>
-              );
-            })}
-          </select>
+              {monthsArr.map((month, index) => {
+                return (
+                  <option key={index} value={month}>
+                    {month}
+                  </option>
+                );
+              })}
+            </select>
 
-          <select
-            defaultValue="Number of days"
-            name="filter"
-            id="numberOfDaysFilter"
-            onChange={(e) => setNumberOfDays(parseInt(e.target.value))}
-          >
-            <option disabled value="Number of days">
-              Number of days
+            <select
+              defaultValue="Number of days"
+              name="filter"
+              id="numberOfDaysFilter"
+              onChange={(e) => setNumberOfDays(parseInt(e.target.value))}
+            >
+              <option disabled value="Number of days">
+                Number of days
             </option>
-            {numberOfDaysArr.map((num, index) => {
-              return (
-                <option key={index} value={num}>
-                  {num}
-                </option>
-              );
-            })}
-          </select>
+              {numberOfDaysArr.map((num, index) => {
+                return (
+                  <option key={index} value={num}>
+                    {num}
+                  </option>
+                );
+              })}
+            </select>
 
-          <select
-            defaultValue="Start day"
-            name="filter"
-            id="startDayFilter"
-            onChange={(e) => setStartDay(e.target.value)}
-          >
-            <option disabled value="Start day">
-              Start day
+            <select
+              defaultValue="Start day"
+              name="filter"
+              id="startDayFilter"
+              onChange={(e) => setStartDay(e.target.value)}
+            >
+              <option disabled value="Start day">
+                Start day
             </option>
-            {daysArr.map((day, index) => {
-              return (
-                <option key={index} value={day}>
-                  {day}
-                </option>
-              );
-            })}
-          </select>
+              {daysArr.map((day, index) => {
+                return (
+                  <option key={index} value={day}>
+                    {day}
+                  </option>
+                );
+              })}
+            </select>
 
-          <select
-            defaultValue="Year"
-            name="filter"
-            id="yearFilter"
-            onChange={(e) => setYear(parseInt(e.target.value))}
-          >
-            <option disabled value="Year">
-              Year
+            <select
+              defaultValue="Year"
+              name="filter"
+              id="yearFilter"
+              onChange={(e) => setYear(parseInt(e.target.value))}
+            >
+              <option disabled value="Year">
+                Year
             </option>
-            {yearsArr.map((year, index) => {
-              return (
-                <option key={index} value={year}>
-                  {year}
-                </option>
-              );
-            })}
-          </select>
-        </div>
+              {yearsArr.map((year, index) => {
+                return (
+                  <option key={index} value={year}>
+                    {year}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
 
-        <div>
-          <button type="submit">Create Calendar</button>
-        </div>
-      </form>
+          <div>
+            <button type="submit">Create Calendar</button>
+          </div>
+        </form>
 
-      <h3 className="errorMessage">{error}</h3>
+        <h3 className="errorMessage">{error}</h3>
+      </div>
+      <Footer/>
     </div>
   );
 }
